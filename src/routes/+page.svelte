0f6d215eb009abfc29d0cdfd76cd34d30081f81c
jia <3108258360@qq.com>
2025-03-11 08:28:57 +0800

    add Fluorescent Lamp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,6 +18,7 @@
 			],
 			linkText: [
 				["Incandescent lamp", "/il"],
+				["Fluorescent Lamp", "/fl"],
 				["Sodium-vapor lamp", "/svl"]
 			],
 			buttonText: "中文"
@@ -35,6 +36,7 @@
 			],
 			linkText: [
 				["白炽灯", "/il"],
+				["荧光灯", "/fl"],
 				["钠灯", "/svl"]
 			],
 			buttonText: "English"
